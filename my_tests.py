d272dea09c5a062d3d04d64e58d2ea62669d384b
mudarisovmur <mudarisovmur@gmail.com>
2023-05-21 15:19:35 +0700

    redirect of workflows

--- my_tests.py
+++ my_tests.py
@@ -1,3 +1,7 @@
 import fastlab
 def test1():
     assert fastlab.sum_two_args(2,2) == 4
+def test3():
+    assert fastlab.sum_two_args(3,2) == 5
+def test4():
+    assert fastlab.sum_two_args("2",2) == 4
