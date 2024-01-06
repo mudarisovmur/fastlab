15ebb9b8e4dff8160b0bf3d917076d29e6d27dd6
mudarisovmur <mudarisovmur@gmail.com>
2024-01-06 15:47:49 +0700

    replace all old files with new

--- fastlab.py
+++ fastlab.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
 app = FastAPI()
+
 def sum_two_args(x,y):
     return x+y
+
 # Hello World route
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+ return {"Hello": "World"}
