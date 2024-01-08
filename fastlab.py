b5e9c9fda967678a721c013e63755c4b6bfd27f3
mudarisovmur <mudarisovmur@gmail.com>
2024-01-08 13:27:41 +0700

    Hot fix 2

--- fastlab.py
+++ fastlab.py
@@ -10,13 +10,8 @@ from fastapi import Form, File, UploadFile
 import matplotlib.pyplot as plt
 from typing import List
 import hashlib
-from PIL import ImageDraw
-from fastapi.responses import RedirectResponse
 
 app = FastAPI()
-def sum_two_args(x,y):
- return x+y
-# Hello World route
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -28,51 +23,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 # возвращаем some.html, сгенерированный из шаблона
 # передав туда одно значение something
-
-
-@app.get("/some_url/{something}", response_class=HTMLResponse)
-async def read_something(request: Request, something: str):
- return templates.TemplateResponse("some.html", {"request": request,
-"something": something})
-def create_some_image(some_difs):
- imx = 200
- imy = 200
- image = np.zeros((imx,imy, 3), dtype=np.int8)
- image[0:imy//2,0:imx//2,0] = some_difs
- image[imy//2:,imx//2:,2] = 240
- image[imy//2:,0:imx//2, 1] = 240
- return image
-# возврат изображения в виде потока медиаданных по URL
-
-
-@app.get("/bimage", response_class=fastapi.responses.StreamingResponse)
-async def b_image(request: Request):
- # рисуем изображение, сюда можете вставить GAN, WGAN сети и т. д
- # взять изображение из массива в Image PIL
- image = create_some_image(100)
- im = Image.fromarray(image, mode="RGB")
- # сохраняем изображение в буфере оперативной памяти
- imgio = io.BytesIO()
- im.save(imgio, 'JPEG')
- imgio.seek(0)
- # Возвращаем изображение в виде mime типа image/jpeg
- return fastapi.responses.StreamingResponse(content=imgio, media_type="image/jpeg")
- # возврат двух изображений в таблице html, одна ячейка ссылается на url bimage
-# другая ячейка указывает на файл из папки static по ссылке
-# при этом файл туда предварительно сохраняется после генерации из массива
-
-
-@app.get("/image", response_class=HTMLResponse)
-async def make_image(request: Request):
- image_n = "image.jpg"
- image_dyn = request.base_url.path + "bimage"
- image_st = request.url_for("static", path=f'/{image_n}')
- image = create_some_image(250)
- im = Image.fromarray(image, mode="RGB")
- im.save(f"./static/{image_n}")
- # передаем в шаблон две переменные, к которым сохранили url
- return templates.TemplateResponse("image.html", {"request": request,
-                                                  "im_st": image_st, "im_dyn": image_dyn})
 
 
 @app.post("/image_form", response_class=HTMLResponse)
