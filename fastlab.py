3c5da453e1ec5f55fb448e4f6770051f449a42ab
mudarisovmur <mudarisovmur@gmail.com>
2024-01-08 12:41:28 +0700

    added main task for lab 1

--- fastlab.py
+++ fastlab.py
@@ -1,22 +1,35 @@
 import fastapi.responses
-import numpy
+import numpy as np
 import io
 from PIL import Image
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi import Form, File, UploadFile
+import matplotlib.pyplot as plt
+from typing import List
+import hashlib
+from PIL import ImageDraw
+from fastapi.responses import RedirectResponse
+
 app = FastAPI()
 def sum_two_args(x,y):
  return x+y
 # Hello World route
-@app.get("/")
-def read_root():
- return {"Hello": "World"}
+
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+  return templates.TemplateResponse("startPage.html",{"request": request})
+
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 # возвращаем some.html, сгенерированный из шаблона
 # передав туда одно значение something
+
+
 @app.get("/some_url/{something}", response_class=HTMLResponse)
 async def read_something(request: Request, something: str):
  return templates.TemplateResponse("some.html", {"request": request,
@@ -24,12 +37,14 @@ async def read_something(request: Request, something: str):
 def create_some_image(some_difs):
  imx = 200
  imy = 200
- image = numpy.zeros((imx,imy, 3), dtype=numpy.int8)
+ image = np.zeros((imx,imy, 3), dtype=np.int8)
  image[0:imy//2,0:imx//2,0] = some_difs
  image[imy//2:,imx//2:,2] = 240
  image[imy//2:,0:imx//2, 1] = 240
  return image
 # возврат изображения в виде потока медиаданных по URL
+
+
 @app.get("/bimage", response_class=fastapi.responses.StreamingResponse)
 async def b_image(request: Request):
  # рисуем изображение, сюда можете вставить GAN, WGAN сети и т. д
@@ -59,44 +74,93 @@ async def make_image(request: Request):
  return templates.TemplateResponse("image.html", {"request": request,
                                                   "im_st": image_st, "im_dyn": image_dyn})
 
-from fastapi import Form, File, UploadFile
-from typing import List
-import hashlib
-from PIL import ImageDraw
+
 @app.post("/image_form", response_class=HTMLResponse)
 async def make_image(request: Request,
-  name_op:str = Form(),
-  number_op:int = Form(),
-  r:int = Form(),
-  g:int = Form(),
-  b:int = Form(),
-  files: List[UploadFile] = File(description="Multiple files as UploadFile")
- ):
- # устанавливаем готовность прорисовки файлов, можно здесь проверить, что файлы вообще есть
-# лучше использовать исключения
- ready = False
- print(len(files))
- if(len(files)>0):
-  if(len(files[0].filename)>0):
-   ready = True
- images = []
- if ready:
-  print([file.filename.encode('utf-8') for file in files])
-  # преобразуем имена файлов в хеш -строку
-  images = ["static/"+hashlib.sha256(file.filename.encode('utf-8')).hexdigest() for file in files]
-  # берем содержимое файлов
-  content = [await file.read() for file in files]
-  # создаем объекты Image типа RGB размером 200 на 200
-  p_images = [Image.open(io.BytesIO(con)).convert("RGB").resize((200,200)) for con in content]
-  # сохраняем изображения в папке static
-  for i in range(len(p_images)):
-   draw = ImageDraw.Draw(p_images[i])
-   # Рисуем красный эллипс с черной окантовкой
-   draw.ellipse((100, 100, 150, 200+number_op), fill=(r,g,b), outline=(0, 0, 0))
-   p_images[i].save("./"+images[i],'JPEG')
- # возвращаем html с параметрами-ссылками на изображения, которые позже будут
- # извлечены браузером запросами get по указанным ссылкам в img src
- return templates.TemplateResponse("forms.html", {"request": request, "ready": ready, "images": images})
+                     r: int = Form(),
+                     g: int = Form(),
+                     files: List[UploadFile] = File(description="Multiple files as UploadFile")
+                     ):
+    # устанавливаем готовность прорисовки файлов, можно здесь проверить, что файлы вообще есть
+    # лучше использовать исключения
+    ready = False
+    print(len(files))
+    if (len(files) > 0):
+        if (len(files[0].filename) > 0):
+            ready = True
+    images = []
+    if ready:
+        print([file.filename.encode('utf-8') for file in files])
+
+        # преобразуем имена файлов в хеш -строку
+        images = ["static/" + hashlib.sha256(file.filename.encode('utf-8')).hexdigest() for file in files]
+
+        # берем содержимое файлов
+        content = [await file.read() for file in files]
+
+        # создаем объекты Image типа RGB
+        p_images = [Image.open(io.BytesIO(con)).convert("RGB") for con in content]
+
+        # Только для первой картинки из загруженных
+        i = 0
+
+        # Создаём новую картинку в локальной переменной
+        shifted_x_image = p_images[i].copy()
+
+        # Получаем ширину и высоту для процентной обработки
+        width = shifted_x_image.width
+        height = shifted_x_image.height
+
+        iter_x = width - width * (r / 100)
+
+        for j in range(0, width):
+
+            for k in range(0, height):
+
+                source_x = 0
+
+                if iter_x < width:
+                    source_x = iter_x
+                else:
+                    source_x = j - width * (r / 100)
+
+                pixel = p_images[i].getpixel((source_x, k))
+
+                shifted_x_image.putpixel((j, k), pixel)
+
+                iter_x += 1
+
+        # Создаём новую картинку на основе уже обработанной по оси X
+        newOutputImage = shifted_x_image.copy()
+
+        for j in range(0, width):
+
+            iter_y = height - height * (g / 100)
+
+            for k in range(0, height):
+
+                source_y = 0
+
+                if iter_y < height:
+                    source_y = iter_y
+                else:
+                    source_y = k - height * (g / 100)
+
+                pixel = shifted_x_image.getpixel((j, source_y))
+
+                newOutputImage.putpixel((j, k), pixel)
+
+                iter_y += 1
+
+        p_images[i] = newOutputImage
+
+        p_images[i].save("./" + images[i], 'JPEG')
+
+    # возвращаем html с параметрами-ссылками на изображения, которые позже будут
+    # извлечены браузером запросами get по указанным ссылкам в img src
+    return templates.TemplateResponse("forms.html", {"request": request, "ready": ready, "images": images})
+
+
 @app.get("/image_form", response_class=HTMLResponse)
 async def make_image(request: Request):
  return templates.TemplateResponse("forms.html", {"request": request})
